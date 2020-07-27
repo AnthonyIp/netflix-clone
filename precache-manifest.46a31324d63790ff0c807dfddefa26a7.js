3a4fdbb8adf3d33b930709755f9fd914e5e714b1
anthony <anthonyip@hotmail.fr>
2020-07-27 20:11:24 +0200

    Updates

---
+++ precache-manifest.46a31324d63790ff0c807dfddefa26a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3164726b6789e9258fc320fc01042d6",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "415ed34ae8c361d177c8",
+    "url": "/netflix-clone/static/css/main.ff1634fa.chunk.css"
+  },
+  {
+    "revision": "b4e748eee4b831d9751a",
+    "url": "/netflix-clone/static/js/2.927cc377.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.927cc377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415ed34ae8c361d177c8",
+    "url": "/netflix-clone/static/js/main.670e86a0.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
